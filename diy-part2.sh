6fa3ede7e227d11d5ff94d8635a0f977791b5d42
Atroc X <93807457+Atroc-X@users.noreply.github.com>
2022-09-29 14:07:58 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,11 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+
+# 修改主机名
+sed -i 's/ImmortalWrt/Atroc-x86/g' package/base-files/files/bin/config_generate
+
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
